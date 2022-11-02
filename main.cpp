252dd81efc9e3ad2b4f87112be1eeecfb7bb6d0b
Balkghar <hugo.germano8@gmail.com>
2022-11-02 17:35:05 +0100

    changement

---
+++ main.cpp
@@ -0,0 +1,56 @@
+//---------------------------------------------------------
+// Fichier      : calendrier.cpp
+// Auteur       : Edwin Häffner & Hugo Germano
+// Date         : 02.11.2022
+
+// But          : Programme qui affiche pour une année [1800 – 2100] saisie par l’utilisateur le calendrier complet en tenant compte des années bissextiles. 
+//
+// Remarque     : 
+//
+// Modifs       :
+//
+// Compilateur  : gcc (Ubuntu 11.2.0-19ubuntu1) 11.2.0
+//---------------------------------------------------------
+
+#include <cstdlib>      // EXIT_SUCCESS
+#include <iostream>     // cout et cin
+#include <iomanip>
+#include <limits>       // numeric_limits<streamsize>
+#include <ctime>        // calendrier
+#include "calendrier.h" //header pour la déclaration de fonction
+
+#define VIDER_BUFFER cin.ignore(numeric_limits<streamsize>::max(),'\n')
+
+using namespace std;
+
+int main() {
+
+   //déclaration des constantes
+   const int   MIN_ANNEE = 1800, MAX_ANNEE = 2100;
+
+   //déclaration des années
+   int annee;
+   char continuer;
+
+   do{
+      do{
+         cout << "Veuillez entrer une année dans l'intervalle entre [" << MIN_ANNEE << "-"<< MAX_ANNEE << "]: ";
+         cin >> annee;
+         VIDER_BUFFER;
+
+      }while(annee < MIN_ANNEE or annee > MAX_ANNEE);
+      
+      //appel la fonction et effect
+      calendrier(annee);
+   
+      do{
+         cout << "Voulez vous recommencer ? [O/N]: ";
+         cin >> continuer;
+         VIDER_BUFFER;
+      }while(continuer != 'O' and continuer != 'N');
+
+   }while(continuer == 'O');
+
+
+   return EXIT_SUCCESS; //retourne un code de succès
+}
