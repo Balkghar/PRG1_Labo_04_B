08e78ecec49c63f3a1dab8f9f852dea9a78db973
Hiroyko <edwin.haffner@heig-vd.ch>
2022-11-02 17:59:28 +0100

    Ajout fonction dans calendrier.cpp

--- calendrier.cpp
+++ calendrier.cpp
@@ -3,3 +3,26 @@
 #include <iomanip>
 #include <limits>       // numeric_limits<streamsize>
 #include <ctime>       // calendrier
+#include "calendrier.h"
+
+int jourAnnee (int annee) {
+
+    
+
+}
+
+bool bissextile (int annee){ //Verification si l'année est bissextile !
+
+    bool bissextile = false;
+
+    if (annee % 400 == 0 or (annee % 4 == 0 and annee % 100 != 0)){
+        bissextile = true;
+    }
+
+    return bissextile;
+}
+
+void affichageCalendrier (int anneeUser){
+    jourAnnee(anneeUser);
+    bissextile(anneeUser);
+}
